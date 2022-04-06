9cd710d38d59107bc5883b1ae78435982a372f71
Dominik Robert <dmnkrobert@gmail.com>
2022-04-06 17:56:15 +0200

    Spiel Loop geändert

--- src/game.js
+++ src/game.js
@@ -1,24 +1,38 @@
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
 var WINDOWS_WIDTH = canvas.width;
 var WIDOWS_HEIGHT = canvas.height;
-var PLAYER_VELOCITY_X = 1;
-var playerX = 50;
-var playerY = 10;
+var PLAYER_VELOCITY_X = 2.5;
+var PLAYER_VELOCITY_Y = 10;
+var playerX = 41;
+var playerY = 0;
 var playerW = 10;
 var playerH = 10;
 var playerColor = '#FFFFFF';
 
 var blockX = 50;
-var blockY = 100;
+var blockY = 300;
 var blockW = 200;
 var blockH = 10;
 
 var gravity_x = 0
 var gravity_y = 1
 
-
+let keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  space: {
+    pressed: false
+  }
+}
 
 function drawEnv(x, y, w, h, color) {
   ctx.beginPath();
@@ -32,23 +46,64 @@ function clear() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function draw() {
+
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'a') {
+    keys.a.pressed = true;
+  }
+  if (event.key === 'd') {
+    keys.d.pressed = true;
+  }
+  if (event.code === 'Space') {
+    keys.space.pressed = true;
+  }
+})
+
+window.addEventListener('keyup', function (event) {
+  if (event.key === 'a') {
+    keys.a.pressed = false;
+  }
+  if (event.key === 'd') {
+    keys.d.pressed = false;
+  }
+  if (event.code === 'Space') {
+    keys.space.pressed = false;
+  }
+})
+
+function collide() {
+  if (
+    playerX < blockX + blockW &&
+    playerX + playerW > blockX &&
+    playerY < blockY + blockH &&
+    playerY + playerH > blockY
+  ) {
+    return true;
+  }
+  return false;
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);
   clear()
   drawEnv(playerX, playerY, playerW, playerH, playerColor);
   drawEnv(blockX, blockY, blockW, blockH, playerColor);
 
-  // playerX = playerX + gravity.x;
-  // playerY = playerY + gravity_y;
+  if (!collide()) {
+    playerY = playerY + gravity_y;
+  }
+
+  if (keys.a.pressed) {
+    playerX = playerX - PLAYER_VELOCITY_X;
+  } else if (keys.d.pressed) {
+    playerX = playerX + PLAYER_VELOCITY_X;
+  }
+  if (keys.space.pressed) {
+    playerY = playerY - PLAYER_VELOCITY_Y;
+  }
 }
 
-
-window.addEventListener('keydown', function (event) {
-  console.log("Key pressed");
-  console.log(event.key);
-})
-
-
-
-setInterval(draw, 10);
+animate();
 
 
